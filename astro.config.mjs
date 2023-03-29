1bac0a042d11a255273e8739c3a6fd639dce1b8e
Josue Tapia <josue.tapia@live.com.ar>
2023-03-29 17:49:57 -0300

    [REMOVE] gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://josttme.github.io',
-  base: '/'
+  integrations: [tailwind()]
 })

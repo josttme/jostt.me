354a8e484b0784d0e79e57eec0e8711707da0b15
Josue Tapia <josue.tapia@live.com.ar>
2023-03-29 03:55:34 -0300

    [MIGRATED] JosttMe Portfolio migrated to Astro

---
+++ tailwind.config.cjs
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#000019',
+        secondary: '#0098d4',
+        textColor: '#f9f9f9'
+      },
+      fontFamily: {
+        Nunito: ['Nunito', 'sans-serif']
+      },
+      transitionTimingFunction: {
+        card: 'cubic-bezier(0.455, 0.03, 0.515, 0.955)'
+      },
+      gridTemplateColumns: {
+        card: 'repeat(auto-fill, minmax(min(100%, 300px), 1fr))',
+        skills: 'repeat(auto-fit, minmax(min(100%, 40px), 1fr))'
+      },
+
+      gridTemplateRows: {
+        card: '30px 1fr  55px'
+      }
+    }
+  },
+  plugins: []
+}

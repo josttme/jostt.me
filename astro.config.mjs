d3433cf330aa7c577bc5fb08d955de720d44cb66
Josue Tapia <josue.tapia@live.com.ar>
2024-02-16 12:12:57 -0300

    :mag: [SEO] SEO update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import astroSitemap from 'astro-sitemap'
 
 import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react()],
+	integrations: [tailwind(), react(), astroSitemap()],
 	vite: {
 		assetsInclude: ['/nunito.regular.woff2']
 	},
